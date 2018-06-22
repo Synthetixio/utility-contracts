1733b3102907b62c5b9f1c43f03dc3d2d4f4c907
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2018-06-22 11:20:41 +1000

    Add utilities and tests.

---
+++ test_settings.py
@@ -0,0 +1,7 @@
+run_test = {
+    'test_LimitedSetup': True,
+    'test_Owned': True,
+    'test_Pausable': True,
+    'test_SafeDecimalMath': True,
+    'test_SelfDestructible': True,
+}
---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+import tests.test_Pausable
+import tests.test_LimitedSetup
+import tests.test_Owned
+import tests.test_SafeDecimalMath
+import tests.test_SelfDestructible
