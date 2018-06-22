414998eeff299cee3995fbc7ad91afb514ff4bab
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2018-06-22 15:16:14 +1000

    Updated Owned contract.

--- contracts/Owned.sol
+++ contracts/Owned.sol
@@ -4,24 +4,24 @@ FILE INFORMATION
 -----------------------------------------------------------------
 
 file:       Owned.sol
-version:    1.1
+version:    1.2
 author:     Anton Jurisevic
             Dominic Romanowski
 
-date:       2018-2-26
+date:       2018-6-22
 
 -----------------------------------------------------------------
 MODULE DESCRIPTION
 -----------------------------------------------------------------
 
-An Owned contract, to be inherited by other contracts.
+A contract with an owner, to be inherited by other contracts.
 Requires its owner to be explicitly set in the constructor.
-Provides an onlyOwner access modifier.
+Provides onlyOwner and onlyNominatedOwner access modifiers.
 
-To change owner, the current owner must nominate the next owner,
-who then has to accept the nomination. The nomination can be
-cancelled before it is accepted by the new owner by having the
-previous owner change the nomination (setting it to 0).
+To change owners, the current owner must nominate the next
+owner, who must then accept the nomination. A nomination can be
+cancelled before it is accepted by nominating the zero
+address, for example.
 
 -----------------------------------------------------------------
 */
@@ -30,8 +30,9 @@ pragma solidity 0.4.24;
 
 /**
  * @title A contract with an owner.
- * @notice Contract ownership can be transferred by first nominating the new owner,
- * who must then accept the ownership, which prevents accidental incorrect ownership transfers.
+ * @notice Contract ownership can be transferred by first
+ * nominating the new owner, who must then accept ownership,
+ * which prevents inadvertent ownership transfers.
  */
 contract Owned {
     address public owner;
@@ -62,11 +63,13 @@ contract Owned {
 
     /**
      * @notice Accept the nomination to be owner.
+     * @dev Only the currently-nominated owner may accept ownership.
      */
     function acceptOwnership()
         external
+        onlyNominatedOwner
     {
-        require(msg.sender == nominatedOwner);
+        // This event must be emitted before the owner is overwritten.
         emit OwnerChanged(owner, nominatedOwner);
         owner = nominatedOwner;
         nominatedOwner = address(0);
@@ -75,6 +78,12 @@ contract Owned {
     modifier onlyOwner
     {
         require(msg.sender == owner);
+        _;
+    }
+
+    modifier onlyNominatedOwner
+    {
+        require(msg.sender == nominatedOwner);
         _;
     }
 
